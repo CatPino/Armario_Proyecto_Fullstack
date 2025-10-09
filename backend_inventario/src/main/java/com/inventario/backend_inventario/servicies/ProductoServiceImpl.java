80f9d843d0050129685c8ad62ec245c82921e394
Catalina Pino <cat.pinosduocuc.cl>
2025-10-09 19:33:30 -0300

    cambio_controller_invemtario

--- backend_inventario/src/main/java/com/inventario/backend_inventario/servicies/ProductoServiceImpl.java
+++ backend_inventario/src/main/java/com/inventario/backend_inventario/servicies/ProductoServiceImpl.java
@@ -8,21 +8,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.inventario.backend_inventario.dto.ProductoRequest;
+import com.inventario.backend_inventario.entities.Categoria;
 import com.inventario.backend_inventario.entities.Producto;
+import com.inventario.backend_inventario.repositories.CategoriaRepositories;
 import com.inventario.backend_inventario.repositories.ProductoRepositories;
 
 
 @Service
 public class ProductoServiceImpl implements ProductoService{
 
- @Autowired
+    @Autowired
     private ProductoRepositories productoRepositories;
+    @Autowired
+    private CategoriaRepositories categoriaRepositories;
 
     @Override
-    public Producto crear(Producto producto){
-        return productoRepositories.save(producto);
-    }
+    public Producto crearProducto(ProductoRequest productoRequest) {
+        Producto nuevoProducto = new Producto();
 
+        nuevoProducto.setNombre(productoRequest.getNombre());
+        nuevoProducto.setDescripcion(productoRequest.getDescripcion());
+        nuevoProducto.setPrecio(productoRequest.getPrecio());
+        nuevoProducto.setStock(productoRequest.getStock());
+        nuevoProducto.setImagenUrl(productoRequest.getImagenUrl());
+        nuevoProducto.setActivo(true);
+
+        Categoria categoria = categoriaRepositories.findById(productoRequest.getCategoriaId())
+                .orElseThrow(() -> new RuntimeException("Categoría no encontrada con ID: " + productoRequest.getCategoriaId()));
+
+        nuevoProducto.setCategoria(categoria);
+        return productoRepositories.save(nuevoProducto);
+    }
 
     @Override
     public Producto obtenerId(Long id) {
@@ -36,11 +53,24 @@ public class ProductoServiceImpl implements ProductoService{
     }
 
     @Override
-    public Producto actualizar(Long id, Producto productoActualizado) {
-        Producto existente = obtenerId(id);
-        existente.setNombre(productoActualizado.getNombre());
+    public Producto actualizarProducto(Long id, ProductoRequest productoRequest) {
+        Producto existente = productoRepositories.findById(id)
+                .orElseThrow(() -> new RuntimeException("Producto no encontrado con ID: " + id));
+
+        existente.setNombre(productoRequest.getNombre());
+        existente.setDescripcion(productoRequest.getDescripcion());
+        existente.setPrecio(productoRequest.getPrecio());
+        existente.setStock(productoRequest.getStock());
+        existente.setImagenUrl(productoRequest.getImagenUrl());
+
+        Categoria categoria = categoriaRepositories.findById(productoRequest.getCategoriaId())
+                .orElseThrow(() -> new RuntimeException("Categoría no encontrada con ID: " + productoRequest.getCategoriaId()));
+
+        existente.setCategoria(categoria);
+
         return productoRepositories.save(existente);
     }
+
     @Override
     public Producto cambiarEstado(Long id, boolean nuevoEstado) {
         Producto producto = obtenerId(id);
@@ -75,18 +105,22 @@ public class ProductoServiceImpl implements ProductoService{
             throw new RuntimeException("Error al subir imagen: " + e.getMessage());
         }
     }
+    
     @Override
     public List<Producto> buscarPorCategoria(String categoriaNombre) {
         return productoRepositories.findByCategoria_Nombre(categoriaNombre);
     }
+
     @Override
     public List<Producto> buscarPorNombre(String nombre) {
         return productoRepositories.findByNombre(nombre);
     }
+
     @Override
     public List<Producto> listarStockBajo() {
         return productoRepositories.findByStockLessThan(5);
     }
+
     @Override
     public List<Producto> listarActivos() {
         return productoRepositories.findByActivoTrue();
