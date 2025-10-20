344f070cd5c604d32cf67dc35fdd70d044aa1682
Catalina Pino <cat.pinosduocuc.cl>
2025-10-20 15:20:17 -0300

    Finalizacion de login

--- FrontendArmario/src/main.jsx
+++ FrontendArmario/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { MantenerSesion } from "../../componentes/MantenerSesion/MantenerSesion"; 
+import { MantenerSesion } from "./componentes/MantenerSesion/MantenerSesion";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
