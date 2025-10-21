ea3848ab543eff67befdfca09ca530b2c5b6123b
Catalina Pino <cat.pinosduocuc.cl>
2025-10-21 13:42:10 -0300

    modificacion_webconfig

--- backend_inventario/src/main/java/com/inventario/backend_inventario/BackendInventarioApplication.java
+++ backend_inventario/src/main/java/com/inventario/backend_inventario/BackendInventarioApplication.java
@@ -2,9 +2,6 @@ package com.inventario.backend_inventario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackendInventarioApplication {
@@ -12,15 +9,4 @@ public class BackendInventarioApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendInventarioApplication.class, args);
 	}
- // ✅ Permitir acceso público a la carpeta /img/
-    @Bean
-    public WebMvcConfigurer webMvcConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addResourceHandlers(ResourceHandlerRegistry registry) {
-                registry.addResourceHandler("/img/**")
-                        .addResourceLocations("file:img/"); 
-            }
-        };
-    }
 }
